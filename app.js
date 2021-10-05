9f9f32812e1c820f08cc8122a940ad5194ac0863
George <caldarescu.george@gmail.com>
2021-10-05 19:40:55 +0200

    start JS

---
+++ app.js
@@ -0,0 +1,12 @@
+// set the variables
+
+const agroConc = document.getElementById('agroConc');
+const odAgro = document.getElementById('odAgro');
+const bradfordConc = document.getElementById('bradfordConc');
+const odBradford = document.getElementById('odBradford')
+const btnAgro = document.getElementById('btnAgro');
+const btnBradford = document.getElementById('btnBradford');
+
+// set the event listener
+
+console.log(agroConc);
