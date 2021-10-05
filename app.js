c0c62b926f70bce472cd4015fba7bda62dcb4208
George <caldarescu.george@gmail.com>
2021-10-05 20:41:17 +0200

    agro works

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // set the variables
 
-const agroConc = document.getElementById('agroConc');
-const odAgro = document.getElementById('odAgro');
-const bradfordConc = document.getElementById('bradfordConc');
-const odBradford = document.getElementById('odBradford')
-const btnAgro = document.getElementById('btnAgro');
-const btnBradford = document.getElementById('btnBradford');
+
+
+
+const btnA = document.getElementById('btnAgro');
+const btnB = document.getElementById('btnBradford');
+const agroCalc = document.getElementById('agroCalc');
+const bradfordCalc = document.getElementById('bradfordCalc');
 
 // set the event listener
 
-console.log(agroConc);
+agroCalc.addEventListener('submit', function(){
+    var Conc = document.getElementById('agroConc').value;
+    var od = document.getElementById('odAgro').value;
+    var a = (od*50)/Conc;
+    var b = (500/a)*4;
+    console.log(b);
+});
+
+bradfordCalc.addEventListener('submit', function(){
+    var conc = document.getElementById('bradfordConc').value;
+    var od = document.getElementById('odBradford').value;
+    var a = ((od *10) * 0.8847 + 0128) * 1000;
+    var b = (conc/a) * 1000;
+    console.log(b);
+});
