faadb059bd781d748fce2e68d3447ed5c8661446
George <caldarescu.george@gmail.com>
2021-10-05 22:02:54 +0200

    finish JS but I still need to work on

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 // set the variables
 
-
-
-
-const btnA = document.getElementById('btnAgro');
-const btnB = document.getElementById('btnBradford');
-const agroCalc = document.getElementById('agroCalc');
-const bradfordCalc = document.getElementById('bradfordCalc');
+let resultAgro = document.getElementById('resultAgro');
+let resultBradford = document.getElementById('resultBradford');
 
 // set the event listener
 
@@ -15,13 +10,25 @@ agroCalc.addEventListener('submit', function(){
     var od = document.getElementById('odAgro').value;
     var a = (od*50)/Conc;
     var b = (500/a)*4;
-    console.log(b);
+    let str = 'Result: ';
+    str += 'add ';
+    str += b;
+    str += 'ul to 2ml of infiltration buffer';
+    resultAgro.textContent = str;
 });
 
 bradfordCalc.addEventListener('submit', function(){
-    var conc = document.getElementById('bradfordConc').value;
     var od = document.getElementById('odBradford').value;
-    var a = ((od *10) * 0.8847 + 0128) * 1000;
+    var a = (((od *10) * 0.8847) + 0.128) * 1000;
+    var conc = document.getElementById('bradfordConc').value;
     var b = (conc/a) * 1000;
-    console.log(b);
+    let stri = 'Result: ';
+    stri =+ Math.round(b);
+    stri += 'ul. And';
+    stri += Math.round(b/5);
+    stri += 'SDS';
+    resultBradford.textContent = stri;
 });
+
+// metti apposto il risultato di Bradford
+// e capisci perche ti manda sopra quando clicchi su submit in bradford
