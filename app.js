62b13cd0f30bca912756608f39bd68fc2fdf99c0
George <caldarescu.george@gmail.com>
2022-09-07 15:59:54 +0200

    start cells counting tool

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 // set the variables
 
-let resultAgro = document.getElementById('resultAgro');
-let resultBradford = document.getElementById('resultBradford');
+let resultAgro = document.getElementById("resultAgro");
+let resultBradford = document.getElementById("resultBradford");
 
 // set the event listener
 
-agroCalc.addEventListener('submit', function(){
-    event.preventDefault()
-    var Conc = document.getElementById('agroConc').value;
-    var od = document.getElementById('odAgro').value;
-    var a = (od*50)/Conc;
-    var b = (500/a)*4;
-    let str = 'Result: ';
-    str += 'add ';
-    str += b;
-    str += 'ul to 2ml of infiltration buffer';
-    resultAgro.textContent = str;
+agroCalc.addEventListener("submit", function () {
+  event.preventDefault();
+  var Conc = document.getElementById("agroConc").value;
+  var od = document.getElementById("odAgro").value;
+  var a = (od * 50) / Conc;
+  var b = (500 / a) * 4;
+  let str = "Result: ";
+  str += "add ";
+  str += b;
+  str += "ul to 2ml of infiltration buffer";
+  resultAgro.textContent = str;
 });
 
-bradfordCalc.addEventListener('submit', function(){
-    event.preventDefault()
-    var od = document.getElementById('odBradford').value;
-    var a = (((od *10) * 0.8847) + 0.128) * 1000;
-    var conc = document.getElementById('bradfordConc').value;
-    var b = (conc/a) * 1000;
-    // let stri = 'Result: ';
-    // stri += 'add ';
-    // stri =+ Math.round(b);
-    // stri += 'ul. And';
-    // stri += Math.round(b/5);
-    // stri += 'SDS';
-    resultBradford.textContent = 'Result: add ' + b.toFixed(2) + 'ul. And ' + (b/5).toFixed(2) + 'ul of SDS';
+bradfordCalc.addEventListener("submit", function () {
+  event.preventDefault();
+  var od = document.getElementById("odBradford").value;
+  var a = (od * 10 * 0.8847 + 0.128) * 1000;
+  var conc = document.getElementById("bradfordConc").value;
+  var b = (conc / a) * 1000;
+  // let stri = 'Result: ';
+  // stri += 'add ';
+  // stri =+ Math.round(b);
+  // stri += 'ul. And';
+  // stri += Math.round(b/5);
+  // stri += 'SDS';
+  resultBradford.textContent =
+    "Result: add " +
+    b.toFixed(2) +
+    "ul. And " +
+    (b / 5).toFixed(2) +
+    "ul of SDS";
 });
 
-// metti apposto il risultato di Bradford
+
+cellsCalc.addEventListener("submit", function () {
+  event.preventDefault();
+  var od = document.getElementById("odCells").value;
+  var a = (od * 10);
+  var b = 
+  let str = "Result: ";
+  str += " ";
+  str += b;
+  str += "ul to 2ml of infiltration buffer";
+  resultAgro.textContent = str;
+});
